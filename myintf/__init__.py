ae3827cc202ac393afbefd2339fbbdb29cac138a
gopikaops <gopikab@fnal.gov>
2024-06-18 13:42:40 -0500

    My interface with read once and set once

---
+++ myintf/__init__.py
@@ -0,0 +1,47 @@
+from badger import interface
+import acsys.dpm
+from acsys.dpm import ItemData
+
+
+class Interface(interface.Interface):
+
+    name = 'myintf'
+
+
+    def get_values(self, drf_list):
+        readings = [None]*len(drf_list)
+        async def wrapper(con):
+            async with acsys.dpm.DPMContext(con, dpm_node='DPM09') as dpm:
+                for i in range(len(drf_list)):
+                    await dpm.add_entry(i, drf_list[i]+'@i')
+                    await dpm.start()
+
+                async for reply in dpm:
+                    readings[reply.tag]=reply.data
+                    if readings.count(None) ==0:
+                        break
+            return readings
+        return acsys.run_client(wrapper)
+            
+        
+
+    def set_values(self, drf_list, value_list, settings_role): #DICT ?
+        async def wrapper(con):
+            #async def set_once(con,drf_list,value_list,settings_role):
+            async with acsys.dpm.DPMContext(con) as dpm:
+                await dpm.enable_settings(role=settings_role)
+                for i, dev in enumerate(drf_list):
+                    #await dpm.add_entry(i, dev+'@i')
+                    await dpm.add_entry(i, dev+'@N')
+                await dpm.start()
+       
+                setpairs = list(enumerate(value_list))
+                await dpm.apply_settings(setpairs)
+                print('settings applied: ',value_list)
+
+            return None
+        return acsys.run_client(wrapper)
+    
+my = Interface()
+my.set_values(drf_list=['Z:CUBE_Z'], value_list=[12], settings_role='testing')
+print(my.get_values(drf_list=['Z:CUBE_Z']))
