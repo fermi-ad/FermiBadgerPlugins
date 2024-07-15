87f4e9e42edca52dc2896170e8f6ebc3c473db33
Jason St. John <stjohn@fnal.gov>
2024-07-15 12:48:02 -0500

    Trying a new way

--- plugins/interfaces/MinD7LMSM_using_Tank5Phase/__init__.py
+++ plugins/interfaces/MinD7LMSM_using_Tank5Phase/__init__.py
@@ -1,6 +1,8 @@
 from badger import interface
-import acsys.dpm
+import acsys.dpm, acsys
 from acsys.dpm import ItemData
+from scanner import read_once
+
 class Interface(interface.Interface):
     name = 'MinD7LMSM_using_Tank5Phase'
     """
@@ -8,7 +10,6 @@ class Interface(interface.Interface):
     observables =
     _variables =
     _observations ="""
-
     # If params not specified, it would be an empty dict
     # Private variables
     _states: dict
@@ -16,29 +17,19 @@ class Interface(interface.Interface):
     def __init__(self, **data):
         super().__init__(**data)
         self._states = {}
+
     def get_values(self, drf_list, sample_event='i'):
         readings = {}
-        async def wrapper(con):
-            async with acsys.dpm.DPMContext(con) as dpm:
-                for i in range(len(drf_list)):
-                    await dpm.add_entry(i, drf_list[i]+f'@{sample_event}')
-                    await dpm.start()
-                async for reply in dpm:
-                    readings[reply.meta['name']]=reply.data
-                    if sorted(readings.keys()) == sorted(drf_list):
-                        break
-                return readings
-        return acsys.run_client(wrapper)
+        results = acsys.run_client(read_once, drf_list=drf_list)
+        for i, name in enumerate(drf_list):
+            readings[name] = results[i]
+        return readings
 
     def set_values(self, drf_dict, settings_role): #DICT ?
-        async def wrapper(con):
-            #async def set_once(con,drf_list,value_list,settings_role):
-            async with acsys.dpm.DPMContext(con) as dpm:
-                await dpm.enable_settings(role=settings_role)
-                for i, key in enumerate(drf_dict.keys()):
-                    await dpm.add_entry(i, key+'@n')
-                await dpm.start()
-                setpairs = list(enumerate(drf_dict.values()))
-                await dpm.apply_settings(setpairs)
-            return None
-        return acsys.run_client(wrapper)
+        drf_list = []
+        value_list = []
+        for key, val in drf_dict.items():
+            drf_list.append(key)
+            value_list.append(val)
+        acsys.run_client(set_once, drf_list=drf_list, value_list=value_list,
+                         settings_role=settings_role) 
