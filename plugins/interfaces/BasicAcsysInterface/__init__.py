9d501a3e97f59d0a2df1416394c4ec72c874f9ad
Jason St. John <stjohn@fnal.gov>
2025-01-16 17:35:38 -0600

    Reading and setting device handling, when names differ.

--- plugins/interfaces/BasicAcsysInterface/__init__.py
+++ plugins/interfaces/BasicAcsysInterface/__init__.py
@@ -14,28 +14,43 @@ class Interface(interface.Interface):
     # If params not specified, it would be an empty dict
     # Private variables
     _states: dict
-
-    read_set_pair_pattern = re.compile("^\(.\:.+,.\:.+\)$")#, aix)
-    
+        
     def __init__(self, **data):
         super().__init__(**data)
         self._states = {}
-    
-    def extract_reading_devices(device_list):
+        self._read_set_pair_pattern = re.compile("^.:.+,.:.+$")
+
+    def extract_reading_devices(self, device_list):
         ret_list = []
         for device in device_list:
-            isreadsetpair = self.read_set_pair_pattern.fullmatch(device)
-            if isreadsetpair: print (f'Found read/set pair: {device} ({isreadsetpair})')
-            
-        
+            isreadsetpair = self._read_set_pair_pattern.fullmatch(device)
+            if isreadsetpair:
+                reading_device = device.split(',')[0]
+                ret_list.append(reading_device)
+            else: ret_list.append(device)
+        return ret_list
+
+    def extract_setting_devices(self, device_dict):
+        ret_dict = {}
+        for device,val in device_dict.items():
+            isreadsetpair = self._read_set_pair_pattern.fullmatch(device)
+            if isreadsetpair:
+                setting_device = device.split(',')[1]
+                ret_dict[setting_device] = val
+            else: ret_dict.ret_dict[device] = val
+        return ret_dict
+
     def get_values(self, drf_list, sample_event='i'):
+        readings_list = self.extract_reading_devices(drf_list)
         readings = {}
-        results = acsys.run_client(read_once, drf_list=drf_list)
+        results = acsys.run_client(read_once, drf_list=readings_list) # FIXME , sample_event=sample_event) 
         for i, name in enumerate(drf_list):
             readings[name] = results[i]
+        print (f'get_values() will return: {readings}')
         return readings
 
-    def set_values(self, drf_dict, settings_role): #DICT ?
+    def set_values(self, drf_dict, settings_role):
+        drf_dict = extract_setting_devices(drf_dict)
         drf_list = []
         value_list = []
         for key, val in drf_dict.items():
