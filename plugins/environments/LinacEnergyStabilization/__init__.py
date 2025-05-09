43cb8d0576cd21b0472767a123a0a13a0f079110
Jason St. John <stjohn@fnal.gov>
2025-05-09 18:27:09 -0500

    Implements reading tolerances: Repeat readings until within tolerance.

--- plugins/environments/LinacEnergyStabilization/__init__.py
+++ plugins/environments/LinacEnergyStabilization/__init__.py
@@ -4,42 +4,50 @@ from badger.errors import BadgerNoInterfaceError
 class Environment(environment.Environment):
     name = "LinacEnergyStabilization"
     variables = {
-        "L:V5QSET": [ -34., -32.],
-        "L:CDPHAS,L:LDPADJ": [90., 360.]
+        "L:V5QSET": [ -38., -32.],
+        "L:CDPHAS,L:LDPADJ,tol2@0.0001": [90., 360.],
+        "L:400DFT": [-1,1],
         #"(L:CDPHAS,L:LDPADJ)": [],
     }
     observables = [
         "B:400DFT",
         "B:400DF2",
-        #"SumSq B:400DFTs"
+        "DummySumSq"
     ]
     sample_event: str='E,15,E,0'
-    settings_role: str='testing'
+    settings_role: str='testing' #'nosettings'
     PID_disables: str='L:KDPHGN'
+    debug: bool=False
     #regulate_point_DFT: float=
     
     def get_variables(self, variable_names: list[str]) -> dict:
         if not self.interface:
             raise BadgerNoInterfaceError
-        return self.interface.get_values(variable_names, self.sample_event) # Interface LinacEnergyStabilization handles (read,set) pairs. 
+        if self.debug: print ('LinacEnergyStabilization asking for variables:', variable_names)
+        return self.interface.get_values(variable_names, self.sample_event) # Interface LinacEnergyStabilization handles (read,set) pairs and optional tolerances.
 
-    def set_variables(self, variable_inputs: dict[str, float]):
+    def set_variables(self, settable_devices: dict[str, float]):
+        if not self.interface:
+            if self.debug: print ("not self.interface: {self.interface}.")
+            raise BadgerNoInterfaceError
+        #set_dict = {}
+        #for k in variable_inputs.keys():
+        #    set_dict[f'{k}.SETTING'] = variable_inputs[k] # JMSJ is this really always the right diction? If so it should move to the Interface.
+        #self.interface.set_values(set_dict, settings_role=self.settings_role)
+        self.interface.set_values(settable_devices, settings_role=self.settings_role)
+
+    def get_observables(self, observable_names: list[str], sample_event=sample_event) -> dict:
         if not self.interface:
             raise BadgerNoInterfaceError
-        set_dict = {}
-        for k in variable_inputs.keys():
-            set_dict[f'{k}.SETTING'] = variable_inputs[k]
-        self.interface.set_values(set_dict, settings_role=self.settings_role)
-
-    def get_observables(self, observable_names: list[str]) -> dict:
-        if not self.interface:
-            raise BadgerNoInterfaceError
-        # return self.interface.get_values(observable_names)
-        observable_outputs = {}
-        for obs in observable_names:
-            value = self.interface.get_value(obs)
-            observable_outputs[obs] = value
-        return observable_outputs
+        if self.debug: print ('get_observables() will ask for values of ', observable_names)
+        return self.interface.get_values(observable_names, sample_event=sample_event)
+        # observable_outputs = {}
+        # for obs in observable_names:
+        #     if self.debug: print (f'get_observables)_ will get value for {obs}')
+        #     if obs == 'DummySumSq': value = 23.
+        #     else: value = self.interface.get_value(obs)
+        #     observable_outputs[obs] = value
+        # return observable_outputs
 
 
 
--- plugins/interfaces/BasicAcsysInterface/__init__.py
+++ plugins/interfaces/BasicAcsysInterface/__init__.py
@@ -3,6 +3,8 @@ import acsys.dpm, acsys
 from acsys.dpm import ItemData
 from scanner import read_once,set_once
 import re
+import numpy as np
+from time import sleep
 
 class Interface(interface.Interface):
     name = 'BasicAcsysInterface'
@@ -14,55 +16,131 @@ class Interface(interface.Interface):
     # If params not specified, it would be an empty dict
     # Private variables
     _states: dict
-        
+    _current_sumsq: float
+    _debug: bool
+    
     def __init__(self, **data):
         super().__init__(**data)
         self._states = {}
+        self._current_sumsq = 0.0
+        self._debug=False
         self._read_set_pair_pattern = re.compile("^.:.+,.:.+")
-        self._read_set_pair_PIDreg_pattern = re.compile("^.:.+,.:.,PID.+$")
+        self._read_set_pair_settle_tol_pattern = re.compile("^.:.+,.:.+,tol.+@*")
 
+    # Handle read/set/[settling tolerance] devices, getting just the reading
     def extract_reading_devices(self, device_list):
         ret_list = []
         for device in device_list:
             isreadsetpair = self._read_set_pair_pattern.fullmatch(device)
-            if isreadsetpair:
+            isreadsettolr = self._read_set_pair_settle_tol_pattern.fullmatch(device)
+            if isreadsetpair or isreadsettolr:
                 reading_device = device.split(',')[0]
                 ret_list.append(reading_device)
             else: ret_list.append(device)
         return ret_list
 
+    # Handle read/set/[settling tolerance] devices, getting just the settable devices
     def extract_setting_devices(self, device_dict):
         ret_dict = {}
         for device,val in device_dict.items():
             isreadsetpair = self._read_set_pair_pattern.fullmatch(device)
+            withSETTING = f'{val}.SETTING' # Needed to make all settings? 
             if isreadsetpair:
                 setting_device = device.split(',')[1]
-                ret_dict[setting_device] = val
-            else: ret_dict[device] = val
+                ret_dict[setting_device] = withSETTING
+            else: ret_dict[device] = withSETTING
         return ret_dict
 
-    def extract_PID_checknums(self, device_dict):
-        PID_checknums = {}
-        for device,val in device_dict:
-            PID_checknums[device] = val
-        return PID_checknums
+    def meets_tolerance(self, buff, tol, debug=False):
+        spread = max(buff) - min(buff)
+        if spread <= tol:
+            if debug: print (f'Spread of {buff} is {spread} <= {tol}.')
+            return True
+        else:
+            if debug: print (f'Spread of {buff} is {spread} but greater than {tol}.')
+            return False
+    
+    # While making settings, helper function to handle those with tolerances (because settling occurs)
+    # "L:CDPHAS,L:LDPADJ,tol2@5.0": reading,setting,tol{MINCOUNT}@{TOLERANCE}
+    #   returns: Dictionaries settled_tols, circ_buffers
+    def extract_PID_tolerances(self, device_dict, debug=False):
+        settled_tols = {} # dict to return: PID_tolerances = {settings device:'2.5', }
+        circ_buffers = {} # of empty array buffers of the tailored lengths
+        for device,val in device_dict.items():
+            if not self._read_set_pair_settle_tol_pattern.fullmatch(device):
+                if debug: print (device,"  did not match pattern; tolerance not specified correctly?")
+                continue
+            reading_dev = device.split(',')[0]
+            #settings_dev = device.split(',')[1]
+            tol = device.split(',')[2].replace('tol','')
+            print (f'extract_PID_tolerances() sees tol: {tol}')
+            bufferlen = int(tol.split('@')[0])
+            tolerance = float(tol.split('@')[1])
+            settled_tols[reading_dev] = tolerance # Use full device? Extract reading device?
+            circ_buffers[reading_dev] = np.empty(bufferlen, dtype=float)
+            circ_buffers[reading_dev][:] = np.nan # Initialize all to NaN
+        return settled_tols, circ_buffers
         
+    # Read values from devices
     def get_values(self, drf_list, sample_event='i'):
         readings_list = self.extract_reading_devices(drf_list)
-        readings = {}
-        results = acsys.run_client(read_once, drf_list=readings_list) # FIXME , sample_event=sample_event) 
-        for i, name in enumerate(drf_list):
-            readings[name] = results[i]
-        print (f'get_values() will return: {readings}')
+        if self._debug: print ('BasicAcsysInterface.get_values() got readings_list: ',readings_list)
+        calcDummySumSq = False
+        if 'DummySumSq' in readings_list:
+            if self._debug: print ('About to ask ACSYS to get readings of ',readings_list)
+            readings = {'DummySumSq': self._current_sumsq}
+        else:
+            readings = {} # dict of returned values
+            results = acsys.run_client(read_once, drf_list=readings_list) # FIXME , sample_event=sample_event)
+            for i, name in enumerate(drf_list):
+                readings[name] = results[i]
+        if self._debug: print (f'BasicAcsysInterface.get_values() will return: {readings}')
         return readings
 
-    def set_values(self, drf_dict, settings_role):
-        PID_checknums = extract_PID_checknums(drf_dict)
-        drf_dict = extract_setting_devices(drf_dict)
-        drf_list = []
-        value_list = []
+    # Set values to devices
+    def set_values(self, drf_dict, settings_role, dont_set=True):
+        print (f'BasicAcsysInterface.set_values() was passed drf_dict: {drf_dict}')
+
+        # Testing option, when not making settings.
+        if settings_role == 'nosettings': 
+            self._current_sumsq = 0.0
+            if self._debug: print ('No settings. drf_dict: ',drf_dict)
+            read_devs = self.extract_reading_devices(drf_dict.keys())
+            newvals = self.get_values(read_devs, sample_event='i')
+            for drf, val in newvals.items():
+                self._current_sumsq += float(val)**2.0
+            if self._debug: print ('After BasicAcsysInterface.set_values() ran, now self._current_sumsq = ', self._current_sumsq)
+            return
+
+        # Need a list of settings devices with the .SETTING suffix appended.
+        setdevs = self.extract_setting_devices(drf_dict)
+        setvals = []
         for key, val in drf_dict.items():
-            drf_list.append(key)
-            value_list.append(val)
-        acsys.run_client(set_once, drf_list=drf_list, value_list=value_list,
-                         settings_role=settings_role) 
+            setvals.append(val)
+        if settings_role!='nosettings': 
+            # Send the setting values to their devices.
+            if not dont_set: acsys.run_client(set_once, drf_list=setvals, value_list=setvals, settings_role=settings_role)
+
+            # Check that any specified tolerances have been met
+            settled_tols, circ_buffers = self.extract_PID_tolerances(drf_dict) # JMSJ Set a unitory-sized buffer for non-toleranced devices?
+            while len(circ_buffers)>0:
+                print ('BasicAcsysInterface.set_values() has circ_buffers: ',circ_buffers)
+                settling_devs = list(circ_buffers.keys())
+                newvals = self.get_values(settling_devs)
+                for i, setdev in enumerate(settling_devs):
+                    found_buff = circ_buffers[setdev]
+                    NaNs_here = np.where(np.isnan(found_buff))
+                    if np.array(NaNs_here).size > 0: # Buffer not full yet? Add the new value just read back for this device.
+                        print (f'Could write new value {newvals[setdev]} to buffer location: {NaNs_here[0][0]}')
+                        circ_buffers[setdev][NaNs_here[0][0]] = newvals[setdev]
+                        # If buffer is full now, do the check for being in tolerance.
+                        buffer_full = np.all(np.where(~np.isnan(circ_buffers[setdev])))
+                        if buffer_full and meets_tolerance(circ_buffers[setdev], settled_tols[setdev], debug=True):
+                            del circ_buffers[setdev] # Ok to do in these loops?
+                    else: # no NaNs; buffer was already full; move in newest value and recheck tolerance
+                        circ_buffers[setdev] = np.roll(circ_buffers[setdev], -1) # move oldest entry to the end, and...
+                        circ_buffers[setdev][-1] = newvals[setdev] #...overwrite with newest value
+                        if self.meets_tolerance(circ_buffers[setdev], settled_tols[setdev], debug=True):
+                            del circ_buffers[setdev] # Ok to do in these loops?
+                    sleep(0.7)
+                    
